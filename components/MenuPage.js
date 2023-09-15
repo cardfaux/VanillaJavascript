db7835f79e9bfc515d024162520f789b8925d324
James Hagood <cardfaux@tutanota.com>
2023-09-15 15:25:51 -0500

    added the stylesheets

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -1,6 +1,24 @@
 export class MenuPage extends HTMLElement {
   constructor() {
     super();
+
+    this.root = this.attachShadow({ mode: 'open' });
+
+    const styles = document.createElement('style');
+    this.root.appendChild(styles);
+
+    async function loadCSS() {
+      try {
+        const request = await fetch('/components/MenuPage.css');
+        const css = await request.text();
+        styles.textContent = css;
+      } catch (error) {
+        console.error(error, 'Error loading CSS');
+      } finally {
+        console.log('Done loading CSS');
+      }
+    }
+    loadCSS();
   }
 
   //? When the component is added to the DOM
@@ -8,7 +26,7 @@ export class MenuPage extends HTMLElement {
     console.log('MenuPage added to the DOM');
     const template = document.getElementById('menu-page-template');
     const content = template.content.cloneNode(true);
-    this.appendChild(content);
+    this.root.appendChild(content);
   }
 }
 customElements.define('menu-page', MenuPage);
