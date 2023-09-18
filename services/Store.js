c23243b911a9a1f13ae7028fffebd17ec3ff43bc
James Hagood <cardfaux@tutanota.com>
2023-09-17 19:40:32 -0500

    added the menu and rendered it on the homepage screen

--- services/Store.js
+++ services/Store.js
@@ -5,5 +5,17 @@ const Store = {
   cart: [],
 };
 
-export default Store;
-//? Back in app.js import the two services as modules and create a global app variable.
+const proxiedStore = new Proxy(Store, {
+  set(target, property, value) {
+    target[property] = value;
+    if (property === 'menu') {
+      window.dispatchEvent(new Event('appmenuchange'));
+    }
+    if (property === 'cart') {
+      window.dispatchEvent(new Event('appcartchange'));
+    }
+    return true;
+  },
+});
+
+export default proxiedStore;
