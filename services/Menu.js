386e60241b97968798dff005bfdd453cf04b7248
James Hagood <cardfaux@tutanota.com>
2023-09-17 20:04:12 -0500

    added the product details page

--- services/Menu.js
+++ services/Menu.js
@@ -9,3 +9,23 @@ export async function loadData() {
     console.log('Done loading data');
   }
 }
+
+export async function getProductById(id) {
+  try {
+    if (app.store.menu == null) {
+      await loadData();
+    }
+    for (let c of app.store.menu) {
+      for (let p of c.products) {
+        if (p.id == id) {
+          return p;
+        }
+      }
+    }
+    return null;
+  } catch (error) {
+    console.error(error, 'Error loading product by ID');
+  } finally {
+    console.log('Done loading product by ID');
+  }
+}
